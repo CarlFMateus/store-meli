a72c7e29b279b33c5cb51013ea3e27edf65dc095
Carl Mateus <carl.italentt@gmail.com>
2022-06-03 20:37:04 -0500

    Add helmet in page Home and add image in tab navigator

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './components';
 
 import { Home, NotFound, ProductDetail, Search } from './pages';
+import { routes } from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='items' element={<Search />} />
-          <Route path='items/:id' element={<ProductDetail />} />
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.items} element={<Search />} />
+          <Route path={`${routes.items}/:id`} element={<ProductDetail />} />
           <Route path='*' element={<NotFound /> } />
         </Routes>
       </Layout>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 import logo from '../../assets/logo_meli.png'
 
@@ -6,9 +7,22 @@ import styles from './styles.module.scss'
 
 export const Home = () => {
   return (
-    <div className={styles.homeContainer}>
-      <h1>Bienvenidos a Mercado Libre</h1>
-      <img src={logo} alt='Mercado libre' />
-    </div>
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Mercado Libre</title>
+        <meta
+          name='description'
+          content="Esta plataforma es una prueba para Mercado Libre" />
+        <meta name='keywords' content='Mercado Libre' />
+        <meta name='og:title' content='Mercado Libre' />
+        <meta name='og:description' content="Esta plataforma es una prueba para Mercado Libre" />
+      </Helmet>
+
+      <div className={styles.homeContainer}>
+        <h1>Bienvenidos a Mercado Libre</h1>
+        <img src={logo} alt='Mercado libre' />
+      </div>
+    </>
   )
 }
